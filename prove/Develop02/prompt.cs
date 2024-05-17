e4f12ce4f78287f5438cdb4cecdc5f6b89982d81
vcane <vcane6@gmail.com>
2024-05-16 21:16:30 -0500

    Prompt class with comments and commented out code

--- prove/Develop02/prompt.cs
+++ prove/Develop02/prompt.cs
@@ -1,15 +1,20 @@
 public class Prompt
 {
-  UsedPrompts oldPrompts = new UsedPrompts();
+  //UsedPrompts oldPrompts = new UsedPrompts();
   public List<string> _prompts = new List<string>
   {
     "What was the strongest emotion that you felt today?",
     "Who was the most interesting person I interacted with today?",
-
+    // "What was the best part of my day?",
+    // "How did I see the hand of the Lord in my life today?",
+    // "If I had one thing I could do over today, what would it be?"
   };
-  //public List<string> _usedPrompts = new List<string>();
+  public List<string> _usedPrompts = new List<string>();
   public Prompt() { }
-  // public string RetrievePrompt()
+
+  // First iteration of the RetrievePrompt method
+  // This iteration repeats prompts
+  // public string RetrievePrompt() 
   // {
   //   Random randomGenerator = new Random();
   //   int promptIndex = randomGenerator.Next(0, _prompts.Count);
@@ -17,9 +22,11 @@ public class Prompt
   //   // Potential improvement is to make it so RetrievePrompt doesn't retrieve duplicate prompts
   // }
 
+
+  // the second iteration of the RetrievePrompt method
+  // this is an attempt to prevent repetition of the prompts
   public string RetrievePrompt()
   {
-
     int promptIndex;
     string prompt = String.Empty;
     bool contLoop = true;
@@ -27,13 +34,14 @@ public class Prompt
     {
       Random randomGenerator = new Random();
       promptIndex = randomGenerator.Next(0, _prompts.Count);
-      if (oldPrompts._usedPrompts.Contains(_prompts[promptIndex]))
+      if (_usedPrompts.Contains(_prompts[promptIndex]))
       {
-        continue;
+        continue; //Potential problem is what happens when both lists match each other
+        // will that cause a bug and crash??
       }
       else
       {
-        oldPrompts._usedPrompts.Add(_prompts[promptIndex]);
+        _usedPrompts.Add(_prompts[promptIndex]);
         prompt = _prompts[promptIndex];
         contLoop = false;
       }
@@ -44,7 +52,5 @@ public class Prompt
 }
 
 /*
-"What was the best part of my day?",
-    "How did I see the hand of the Lord in my life today?",
-    "If I had one thing I could do over today, what would it be?"
+
 */
