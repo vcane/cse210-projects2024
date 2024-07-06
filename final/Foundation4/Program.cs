25a322e7fdc9098f28b08b7912d2deda1c0f9427
vcane <vcane6@gmail.com>
2024-07-05 19:28:29 -0500

    updated plantuml URL for 2, created URL for 3 and 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,12 @@
 using System;
 
+// plantuml URL
+// //www.plantuml.com/plantuml/dpng/lP9BJiCm48RtFiKWDebeBX2nAAWR92GgUW0qIQPWoNOgUrmK7dUdaqSeoO8keDkP_p5VucLWz1ojKHVyGfPWhmkFZjLDoNglkO7hZoo3n-YSTi-JwOQHTUtGDxUwR4f3q_shjlPWkyTTk7FhkZPAbGP3W46dtXLSmbE5J3a4zj8X0-ptg-ZRYXoqOwNWQdGcdHh4kf0jJ3rfgr_fmEXAIj8SjgP6bk5cHrJzcAnnAedMejTlD1QcuuMV0qR_MOGYEr3qLNsAr0-AtwzTT2OOGbB8eS10IMVvGxZX-ldlwpc211n3p8IduxHPvs2PFxKEnaLRa7_OokBx5Stbfo39z3ZlNxcMvAfepHS0
+
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation4 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Foundation4 World!");
+  }
 }
