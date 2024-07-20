be1c189d7d243596002861fb29245461a9134629
vcane <vcane6@gmail.com>
2024-07-20 13:50:33 -0500

    removed temp variables, created orderList, cleaned up commented code

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -2,45 +2,35 @@ class Program
 {
   static void Main(string[] args)
   {
-    // Address address1 = new Address("123 Main", "Anytown", "Kansas", "66610", "US");
-    // Address address2 = new Address("246 Main", "Toronto", "Quebec", "12345", "Canada");
+    List<Order> orderList = new List<Order>()
+    {
+      new Order(5, 35, new Customer("Virgil Cane", new Address("123 Main", "Anytown", "Kansas", "66610", "US"))),
+      new Order(5, 35, new Customer("Joe Eh", new Address("246 Main", "Toronto", "Quebec", "12345", "Canada")))
+    };
 
-    //Customer customer1 = new Customer("Virgil Cane", address1);
-    //Customer customer2 = new Customer("Joe Eh", address2);
-    //new Customer("Joe Eh", new Address("246 Main", "Toronto", "Quebec", "12345", "Canada"))
-
-    // Each order consists of the usShipping, foreignShipping, and customer info which includes name and address
-    Order order1 = new Order(5, 35, new Customer("Virgil Cane", new Address("123 Main", "Anytown", "Kansas", "66610", "US")));
-    // Product product1 = new Product("Plush Cat", "Cat1234", 5, 5);
-    // Product product2 = new Product("Plush Dog", "Dog123", 6, 6);
-
-    order1.GetProductsList().Add(new Product("Plush Cat", "Cat1234", 5, 5));
-    order1.GetProductsList().Add(new Product("Plush Dog", "Dog123", 6, 6));
-
-    Order order2 = new Order(5, 35, new Customer("Joe Eh", new Address("246 Main", "Toronto", "Quebec", "12345", "Canada")));
-    // Product product3 = new Product("Red Power Ranger", "PR123", 10, 2);
-    // Product product4 = new Product("Pink Power Ranger", "PR456", 10, 3);
-
-    order2.GetProductsList().Add(new Product("Red Power Ranger", "PR123", 10, 2));
-    order2.GetProductsList().Add(new Product("Pink Power Ranger", "PR456", 10, 3));
+    // adding products to orders
+    orderList[0].AddProduct(new Product("Plush Cat", "Cat1234", 5, 5));
+    orderList[0].AddProduct(new Product("Plush Dog", "Dog123", 6, 6));
+    orderList[1].AddProduct(new Product("Red Power Ranger", "PR123", 10, 2));
+    orderList[1].AddProduct(new Product("Pink Power Ranger", "PR456", 10, 3));
 
     Console.WriteLine("Order for customer 1:");
     Console.WriteLine($"\nCustomer 1 shipping label: ");
-    Console.WriteLine(order1.RenderShippingLabel());//address1, customer1
+    Console.WriteLine(orderList[0].RenderShippingLabel());
 
     Console.WriteLine("\nHere is packing label for order 1:");
-    order1.GeneratePackingLabel(order1.GetProductsList());
+    orderList[0].GeneratePackingLabel();
 
-    Console.WriteLine($"\nThe total cost for order 1 is ${order1.CalculateTotalCost(order1.GetProductsList())}");
+    Console.WriteLine($"\nThe total cost for order 1 is ${orderList[0].CalculateTotalCost()}");
 
     Console.WriteLine("\nOrder for customer 2:");
 
     Console.WriteLine("\nCustomer 2 shipping label:");
-    Console.WriteLine(order2.RenderShippingLabel()); //address2, customer2
+    Console.WriteLine(orderList[1].RenderShippingLabel());
 
     Console.WriteLine("\nHere is packing label for order 2:");
-    order2.GeneratePackingLabel(order2.GetProductsList());
+    orderList[1].GeneratePackingLabel();
 
-    Console.WriteLine($"\nThe total cost for order 2 is ${order2.CalculateTotalCost(order2.GetProductsList())}");
+    Console.WriteLine($"\nThe total cost for order 2 is ${orderList[1].CalculateTotalCost()}");
   }
 }
